90f9fb92ee6b259b0677296ee74a162abaae3408
jefferson-lam <jefferson.lam@polymtl.ca>
2021-09-22 08:58:14 -0400

    Replace deprecated methods

--- client/src/app/components/canvas/canvas.component.spec.ts
+++ client/src/app/components/canvas/canvas.component.spec.ts
@@ -1,9 +1,9 @@
-import { Renderer2 } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DrawingService } from '../../services/drawing/drawing.service';
-import { CanvasComponent } from './canvas.component';
+import { Renderer2 } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DrawingService } from "../../services/drawing/drawing.service";
+import { CanvasComponent } from "./canvas.component";
 
-describe('CanvasComponent', () => {
+describe("CanvasComponent", () => {
   let component: CanvasComponent;
   let fixture: ComponentFixture<CanvasComponent>;
   let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
@@ -13,38 +13,45 @@ describe('CanvasComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CanvasComponent],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    const rendererSpyTemp = jasmine.createSpyObj('Renderer2',
-      ['createElement', 'setProperty', 'setAttribute', 'appendChild', 'setStyle', 'removeChild']);
-    let spyDrawingService = jasmine.createSpyObj('DrawingService', ['']);
+    const rendererSpyTemp = jasmine.createSpyObj("Renderer2", [
+      "createElement",
+      "setProperty",
+      "setAttribute",
+      "appendChild",
+      "setStyle",
+      "removeChild",
+    ]);
+    let spyDrawingService = jasmine.createSpyObj("DrawingService", [""]);
     spyDrawingService = {
       ...spyDrawingService,
       renderer: rendererSpyTemp,
     };
     TestBed.configureTestingModule({
-      providers: [{ provide: Renderer2, useValue: rendererSpyTemp }, { provide: DrawingService, useValue: spyDrawingService }],
+      providers: [
+        { provide: Renderer2, useValue: rendererSpyTemp },
+        { provide: DrawingService, useValue: spyDrawingService },
+      ],
       declarations: [CanvasComponent],
     });
     drawingServiceSpy = TestBed.get(DrawingService);
 
     fixture = TestBed.createComponent(CanvasComponent);
-    // tslint:disable-next-line: deprecation
-    rendererSpy = fixture.componentRef.injector.get(Renderer2);
-    spyOn(rendererSpy, 'removeChild');
-    spyOn(rendererSpy, 'appendChild');
+    rendererSpy = TestBed.get(Renderer2);
+    spyOn(rendererSpy, "removeChild");
+    spyOn(rendererSpy, "appendChild");
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should return dimension value when service created value is true', () => {
+  it("should return dimension value when service created value is true", () => {
     drawingServiceSpy.isCreated = true;
     drawingServiceSpy.height = 100;
     drawingServiceSpy.width = 100;
@@ -52,23 +59,25 @@ describe('CanvasComponent', () => {
     expect(component.width).toEqual(100);
   });
 
-  it('should return 0 when service created value is false ', () => {
+  it("should return 0 when service created value is false ", () => {
     drawingServiceSpy.isCreated = false;
     expect(component.height).toEqual(0);
     expect(component.width).toEqual(0);
   });
 
-  it('should return background alpha value', () => {
+  it("should return background alpha value", () => {
     drawingServiceSpy.alpha = 9;
     expect(component.backgroundAlpha).toEqual(9);
   });
 
-  it('should return drawingServicecolor value', () => {
-    spyOnProperty(drawingServiceSpy, 'rgbaColorString').and.returnValue('rgba(255,0,0,1)');
-    expect(component.backgroundColor).toEqual('rgba(255,0,0,1)');
+  it("should return drawingServicecolor value", () => {
+    spyOnProperty(drawingServiceSpy, "rgbaColorString").and.returnValue(
+      "rgba(255,0,0,1)"
+    );
+    expect(component.backgroundColor).toEqual("rgba(255,0,0,1)");
   });
 
-  it('should return value of isDrawingCreated ', () => {
+  it("should return value of isDrawingCreated ", () => {
     drawingServiceSpy.isCreated = true;
     expect(component.isDrawingCreated).toEqual(true);
 
@@ -76,23 +85,34 @@ describe('CanvasComponent', () => {
     expect(component.isDrawingCreated).toEqual(false);
   });
 
-  it(' ngAfterView should be called with no svg', () => {
-    const el: SVGElement = document.createElementNS('svg', 'svg') as SVGElement;
+  it(" ngAfterView should be called with no svg", () => {
+    const el: SVGElement = document.createElementNS("svg", "svg") as SVGElement;
     component.ngAfterViewInit();
     drawingServiceSpy.drawingEmit.emit(el);
     fixture.detectChanges();
-    expect(rendererSpy.appendChild).toHaveBeenCalledWith(component.canvasDiv.nativeElement, el);
+    expect(rendererSpy.appendChild).toHaveBeenCalledWith(
+      component.canvasDiv.nativeElement,
+      el
+    );
   });
 
-  it(' ngAfterView should be called with svg', () => {
-    const previousel: SVGElement = document.createElementNS('svg', 'svg') as SVGElement;
-    const el: SVGElement = document.createElementNS('svg', 'svg') as SVGElement;
+  it(" ngAfterView should be called with svg", () => {
+    const previousel: SVGElement = document.createElementNS(
+      "svg",
+      "svg"
+    ) as SVGElement;
+    const el: SVGElement = document.createElementNS("svg", "svg") as SVGElement;
     component.svg = previousel;
     component.ngAfterViewInit();
     drawingServiceSpy.drawingEmit.emit(el);
     fixture.detectChanges();
-    expect(rendererSpy.removeChild).toHaveBeenCalledWith(component.canvasDiv.nativeElement, previousel);
-    expect(rendererSpy.appendChild).toHaveBeenCalledWith(component.canvasDiv.nativeElement, el);
+    expect(rendererSpy.removeChild).toHaveBeenCalledWith(
+      component.canvasDiv.nativeElement,
+      previousel
+    );
+    expect(rendererSpy.appendChild).toHaveBeenCalledWith(
+      component.canvasDiv.nativeElement,
+      el
+    );
   });
-
 });
--- client/src/app/services/tools/eraser-tool/eraser-tool.service.spec.ts
+++ client/src/app/services/tools/eraser-tool/eraser-tool.service.spec.ts
@@ -1,12 +1,12 @@
-import { EventEmitter } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { CommandInvokerService } from '../../command-invoker/command-invoker.service';
-import { DrawingService } from '../../drawing/drawing.service';
-import { OffsetManagerService } from '../../offset-manager/offset-manager.service';
-import { RendererProviderService } from '../../renderer-provider/renderer-provider.service';
-import { EraserToolService } from './eraser-tool.service';
+import { EventEmitter } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+import { CommandInvokerService } from "../../command-invoker/command-invoker.service";
+import { DrawingService } from "../../drawing/drawing.service";
+import { OffsetManagerService } from "../../offset-manager/offset-manager.service";
+import { RendererProviderService } from "../../renderer-provider/renderer-provider.service";
+import { EraserToolService } from "./eraser-tool.service";
 
-describe('EraserToolService', () => {
+describe("EraserToolService", () => {
   let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
   let offsetManagerSpy: jasmine.SpyObj<OffsetManagerService>;
   let service: EraserToolService;
@@ -17,17 +17,26 @@ describe('EraserToolService', () => {
   let rendererProvider: RendererProviderService;
 
   beforeEach(() => {
-    let spyDrawingService = jasmine.createSpyObj('DrawingService', ['getObjectList', 'removeObject', 'addObject']);
+    let spyDrawingService = jasmine.createSpyObj("DrawingService", [
+      "getObjectList",
+      "removeObject",
+      "addObject",
+    ]);
     spyDrawingService = {
       ...spyDrawingService,
-      drawing: document.createElementNS('svg', 'svg'),
+      drawing: document.createElementNS("svg", "svg"),
       drawingEmit: new EventEmitter(),
     };
 
     TestBed.configureTestingModule({
       providers: [
         { provide: DrawingService, useValue: spyDrawingService },
-        { provide: OffsetManagerService, useValue: jasmine.createSpyObj('OffsetManagerService', ['offsetFromMouseEvent']) },
+        {
+          provide: OffsetManagerService,
+          useValue: jasmine.createSpyObj("OffsetManagerService", [
+            "offsetFromMouseEvent",
+          ]),
+        },
         RendererProviderService,
       ],
     });
@@ -35,34 +44,36 @@ describe('EraserToolService', () => {
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 15, y: 15 });
     drawingServiceSpy = TestBed.get(DrawingService);
     rendererProvider = TestBed.get(RendererProviderService);
-    drawing = rendererProvider.renderer.createElement('svg', 'svg');
-    rendererProvider.renderer.setAttribute(drawing, 'width', '200px');
-    rendererProvider.renderer.setAttribute(drawing, 'height', '100px');
-    rect1 = rendererProvider.renderer.createElement('rect', 'svg');
-    rendererProvider.renderer.setAttribute(rect1, 'width', '30px');
-    rendererProvider.renderer.setAttribute(rect1, 'height', '30px');
-    rendererProvider.renderer.setAttribute(rect1, 'x', '12px');
-    rendererProvider.renderer.setAttribute(rect1, 'y', '12px');
-    rendererProvider.renderer.setAttribute(rect1, 'name', 'rectangle');
-    rendererProvider.renderer.setProperty(rect1, 'id', '2');
+    drawing = rendererProvider.renderer.createElement("svg", "svg");
+    rendererProvider.renderer.setAttribute(drawing, "width", "200px");
+    rendererProvider.renderer.setAttribute(drawing, "height", "100px");
+    rect1 = rendererProvider.renderer.createElement("rect", "svg");
+    rendererProvider.renderer.setAttribute(rect1, "width", "30px");
+    rendererProvider.renderer.setAttribute(rect1, "height", "30px");
+    rendererProvider.renderer.setAttribute(rect1, "x", "12px");
+    rendererProvider.renderer.setAttribute(rect1, "y", "12px");
+    rendererProvider.renderer.setAttribute(rect1, "name", "rectangle");
+    rendererProvider.renderer.setProperty(rect1, "id", "2");
     rendererProvider.renderer.appendChild(drawing, rect1);
 
-    const g = rendererProvider.renderer.createElement('g', 'svg');
-    rendererProvider.renderer.setProperty(g, 'id', '1');
-    rect2 = rendererProvider.renderer.createElement('rect', 'svg');
-    rendererProvider.renderer.setAttribute(rect2, 'width', '60px');
-    rendererProvider.renderer.setAttribute(rect2, 'height', '60px');
-    rendererProvider.renderer.setAttribute(rect2, 'x', '5px');
-    rendererProvider.renderer.setAttribute(rect2, 'y', '5px');
-    rendererProvider.renderer.setAttribute(rect2, 'name', 'rectangle');
-    rendererProvider.renderer.setProperty(rect2, 'id', '1');
+    const g = rendererProvider.renderer.createElement("g", "svg");
+    rendererProvider.renderer.setProperty(g, "id", "1");
+    rect2 = rendererProvider.renderer.createElement("rect", "svg");
+    rendererProvider.renderer.setAttribute(rect2, "width", "60px");
+    rendererProvider.renderer.setAttribute(rect2, "height", "60px");
+    rendererProvider.renderer.setAttribute(rect2, "x", "5px");
+    rendererProvider.renderer.setAttribute(rect2, "y", "5px");
+    rendererProvider.renderer.setAttribute(rect2, "name", "rectangle");
+    rendererProvider.renderer.setProperty(rect2, "id", "1");
     rendererProvider.renderer.appendChild(g, rect2);
     rendererProvider.renderer.appendChild(drawing, g);
     drawingServiceSpy.drawing = drawing;
     objList = new Map<number, SVGElement>();
     objList.set(2, rect1);
     objList.set(1, g);
-    drawingServiceSpy.removeObject.and.callFake((id: number) => objList.delete(id));
+    drawingServiceSpy.removeObject.and.callFake((id: number) =>
+      objList.delete(id)
+    );
     drawingServiceSpy.addObject.and.callFake((svgEl: SVGElement) => {
       objList.set(Number(svgEl.id), svgEl);
       return Number(svgEl.id);
@@ -72,109 +83,109 @@ describe('EraserToolService', () => {
     service.parameters.patchValue({ eraserSize: 20 });
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeDefined();
     expect(service.eraser).toBeDefined();
   });
 
-  it('#onMove should move the eraser and not erase anything', () => {
+  it("#onMove should move the eraser and not erase anything", () => {
     service.pickupTool();
-    const previousX = service.eraser.getAttribute('x');
-    const previousY = service.eraser.getAttribute('y');
-    service.onMove(new MouseEvent('mousemouve'));
-    expect(service.eraser.getAttribute('x')).not.toEqual(previousX);
-    expect(service.eraser.getAttribute('y')).not.toEqual(previousY);
-    service.onMove(new MouseEvent('mousemouve'));
+    const previousX = service.eraser.getAttribute("x");
+    const previousY = service.eraser.getAttribute("y");
+    service.onMove(new MouseEvent("mousemouve"));
+    expect(service.eraser.getAttribute("x")).not.toEqual(previousX);
+    expect(service.eraser.getAttribute("y")).not.toEqual(previousY);
+    service.onMove(new MouseEvent("mousemouve"));
     expect(objList.size).toEqual(2);
   });
 
-  it('#onPresse, #onMove, #onRelease should move the eraser and delete the drawings', () => {
+  it("#onPresse, #onMove, #onRelease should move the eraser and delete the drawings", () => {
     service.pickupTool();
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 0, y: 0 });
-    service.onPressed(new MouseEvent('onpress', { button: 0 }));
+    service.onPressed(new MouseEvent("onpress", { button: 0 }));
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 15, y: 15 });
-    service.onMove(new MouseEvent('mousemouve'));
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onMove(new MouseEvent("mousemouve"));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(0);
   });
 
-  it('#onPresse, #onRelease should move the eraser an delete the drawings', () => {
+  it("#onPresse, #onRelease should move the eraser an delete the drawings", () => {
     service.pickupTool();
-    service.onPressed(new MouseEvent('onpress', { button: 2 }));
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onPressed(new MouseEvent("onpress", { button: 2 }));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(0);
   });
 
-  it('#onPresse, #dropTool should not erase anything', () => {
+  it("#onPresse, #dropTool should not erase anything", () => {
     service.pickupTool();
-    service.onPressed(new MouseEvent('onpress', { button: 2 }));
+    service.onPressed(new MouseEvent("onpress", { button: 2 }));
     service.dropTool();
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(2);
   });
 
-  it('#pickupTool should add the eraser', () => {
+  it("#pickupTool should add the eraser", () => {
     service.pickupTool();
     expect(drawing.children.length).toEqual(3);
   });
 
-  it('#onKeyDown, #onKeyUp, #changeTool should do nothing', () => {
-    service.onKeyDown(new KeyboardEvent('onkeydown'));
-    service.onKeyUp(new KeyboardEvent('onkeyup'));
-    service.onRelease(new MouseEvent('onkeyup'));
+  it("#onKeyDown, #onKeyUp, #changeTool should do nothing", () => {
+    service.onKeyDown(new KeyboardEvent("onkeydown"));
+    service.onKeyUp(new KeyboardEvent("onkeyup"));
+    service.onRelease(new MouseEvent("onkeyup"));
     expect().nothing();
   });
 
-  it('#onPresse, #onMove, #onRelease should do nothing without pickup', () => {
+  it("#onPresse, #onMove, #onRelease should do nothing without pickup", () => {
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 0, y: 0 });
-    service.onPressed(new MouseEvent('onpress', { button: 0 }));
+    service.onPressed(new MouseEvent("onpress", { button: 0 }));
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 15, y: 15 });
-    service.onMove(new MouseEvent('mousemouve'));
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onMove(new MouseEvent("mousemouve"));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(2);
   });
 
-  it('#onRelease if not pressed should not erase anything', () => {
+  it("#onRelease if not pressed should not erase anything", () => {
     service.pickupTool();
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(2);
   });
 
-  it('#onPresse, #onMove, #onRelease should move the eraser and not do anything on wrong button', () => {
+  it("#onPresse, #onMove, #onRelease should move the eraser and not do anything on wrong button", () => {
     service.pickupTool();
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 0, y: 0 });
-    service.onPressed(new MouseEvent('onpress', { button: 7 }));
-    service.onKeyUp(new KeyboardEvent('onkeyup'));
+    service.onPressed(new MouseEvent("onpress", { button: 7 }));
+    service.onKeyUp(new KeyboardEvent("onkeyup"));
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: 15, y: 15 });
-    service.onMove(new MouseEvent('mousemouve'));
-    service.onRelease(new MouseEvent('onrelease'));
+    service.onMove(new MouseEvent("mousemouve"));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(2);
   });
 
-  it('#onPresse, #onRelease should not contain any element', () => {
+  it("#onPresse, #onRelease should not contain any element", () => {
     service.pickupTool();
-    rendererProvider.renderer.setStyle(rect1, 'strokeWidth', '10px');
+    rendererProvider.renderer.setStyle(rect1, "strokeWidth", "10px");
     service.parameters.patchValue({ eraserSize: 1 });
     offsetManagerSpy.offsetFromMouseEvent.and.returnValue({ x: -10, y: -10 });
-    spyOn(service.eraser, 'getBoundingClientRect')
-      .and.returnValue({ left: -100, bottom: -100, top: -200, right: -50, width: 20, height: 20 });
-    service.onPressed(new MouseEvent('onpress', { button: 0 }));
-    service.onRelease(new MouseEvent('onrelease'));
+    // spyOn(service.eraser, 'getBoundingClientRect')
+    //   .and.returnValue({ left: -100, bottom: -100, top: -200, right: -50, width: 20, height: 20 });
+    service.onPressed(new MouseEvent("onpress", { button: 0 }));
+    service.onRelease(new MouseEvent("onrelease"));
     expect(objList.size).toEqual(2);
   });
 
-  it('should resize', () => {
+  it("should resize", () => {
     service.pickupTool();
-    const previousWidth = service.eraser.getAttribute('width');
+    const previousWidth = service.eraser.getAttribute("width");
     service.parameters.patchValue({ eraserSize: 30 });
-    expect(service.eraser.getAttribute('width')).not.toEqual(previousWidth);
+    expect(service.eraser.getAttribute("width")).not.toEqual(previousWidth);
   });
 
-  it('should reset its list on command invoke', () => {
+  it("should reset its list on command invoke", () => {
     service.pickupTool();
     const commandInvoker = TestBed.get(CommandInvokerService);
-    const spy = spyOn(service, 'reset');
-    commandInvoker.commandCallEmitter.emit('undo');
+    const spy = spyOn(service, "reset");
+    commandInvoker.commandCallEmitter.emit("undo");
     expect(spy).toHaveBeenCalled();
     spy.and.callThrough();
   });
