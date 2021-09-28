13c761941dc4867bc3df98265947596ce84b221a
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-09-28 11:08:05 -0400

    Restructured base socket class methods for easier implementations in child classes

--- server/app/interfaces/socket.interface.ts
+++ server/app/interfaces/socket.interface.ts
@@ -1,22 +1,42 @@
-import { injectable } from 'inversify';
+import { injectable } from "inversify";
 import { Namespace, Server, Socket } from "socket.io";
-import { ROOM_EVENT_NAME } from "@app/services/socket-constants"; 
-import 'reflect-metadata';
+import {
+  ROOM_EVENT_NAME,
+  CONNECTION_EVENT_NAME,
+  DISCONNECTION_EVENT_NAME,
+} from "@app/services/socket-constants";
+import "reflect-metadata";
 
 @injectable()
 export abstract class SocketServiceInterface {
-    protected namespace: Namespace;
+  protected namespace: Namespace;
 
-    init(io: Server, namespaceName: string) {
-        this.namespace = io.of(namespaceName);
-        this.setupSocketOns();
-    }
+  // Initialisation du socket namespace ainsi que toutes les
+  // réponses socket.
+  init(io: Server, namespaceName: string) {
+    this.namespace = io.of(namespaceName);
+    this.namespace.on(CONNECTION_EVENT_NAME, (socket: Socket) => {
+      console.log(`${namespaceName} socket user has connected.`);
+      this.setOnRoom(socket);
+      this.setupSocketOns(socket);
+    });
+  }
 
-    protected abstract setupSocketOns(): void;
+  // Défini tous les socket.on du socket en question.
+  protected abstract setupSocketOns(socket: Socket): void;
 
-    protected setOnRoom(socket:Socket){
-       socket.on(ROOM_EVENT_NAME, (roomName: string) => {
-            socket.join(roomName);
-        });
-    }
+  // Configuration du room en fonction du nom du room envoyé par le client
+  protected setOnRoom(socket: Socket) {
+    socket.on(ROOM_EVENT_NAME, (roomName: string) => {
+      console.log(`User has joined room ${roomName}`);
+      socket.join(roomName);
+    });
+  }
+
+  // Méthode par défaut sur déconnexion d'un utilisateur sur le namespace.
+  protected setOnDisconnect(socket: Socket) {
+    socket.on(DISCONNECTION_EVENT_NAME, () => {
+      console.log(`User from namespace ${this.namespace} disconnected`);
+    });
+  }
 }
