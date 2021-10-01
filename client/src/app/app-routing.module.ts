93c813f9b1ea867931c5783b9374116869dfda55
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-10-01 02:27:25 -0400

    Removed certain elements for prototype

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./authentication/auth.guard";
 import { PageNotFoundComponent } from "./components/error/page-not-found/page-not-found.component";
-import { HomeComponent } from "./components/home/home.component";
-import { SidenavComponent } from "./components/sidenav/sidenav.component";
+// import { HomeComponent } from "./components/home/home.component";
+// import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { ChatComponent } from "./components/chat/chat.component";
 
 const routes: Routes = [
-  { path: "drawing", component: SidenavComponent, canActivate: [AuthGuard] },
-  { path: "chat", component: ChatComponent, canActivate: [AuthGuard] },
-  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  // { path: "drawing", component: SidenavComponent, canActivate: [AuthGuard] },
+  { path: "", component: ChatComponent, canActivate: [AuthGuard] },
+  // { path: "", component: HomeComponent, canActivate: [AuthGuard] },
   { path: "**", component: PageNotFoundComponent },
 ];
 
