e04c22adfa93ee84d9327486e51cca29a88a38f5
keshav <kdooleea@yahoo.ca>
2021-10-08 21:04:32 -0400

    rebuilt login UI and updated logics

--- mobile/app/src/main/java/com/example/colorimagemobile/ui/login/LoginActivity.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/login/LoginActivity.kt
@@ -2,10 +2,12 @@ package com.example.colorimagemobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
 import com.example.colorimagemobile.models.DataWrapper
 import com.example.colorimagemobile.models.HTTPResponseModel
@@ -18,12 +20,21 @@ import com.example.colorimagemobile.utils.CommonFun.Companion.closeKeyboard
 import com.example.colorimagemobile.utils.CommonFun.Companion.onEnterKeyPressed
 import com.example.colorimagemobile.utils.CommonFun.Companion.printToast
 import com.example.colorimagemobile.utils.CommonFun.Companion.redirectTo
+import com.example.colorimagemobile.utils.CommonFun.Companion.toggleButton
+import com.example.colorimagemobile.utils.Constants.Companion.DEBUG_KEY
 import com.example.colorimagemobile.utils.Constants.Companion.SHARED_TOKEN_KEY
 import com.example.colorimagemobile.utils.Constants.Companion.SHARED_USERNAME_KEY
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginViewModel: LoginActivityViewModel
     private lateinit var sharedPreferencesService: SharedPreferencesService
+    private lateinit var usernameEditText: TextInputEditText
+    private lateinit var passwordEditText: TextInputEditText
+    private lateinit var usernameInputLayout: TextInputLayout
+    private lateinit var passwordInputLayout: TextInputLayout
+    private lateinit var loginBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,33 +42,49 @@ class LoginActivity : AppCompatActivity() {
 
         loginViewModel = ViewModelProvider(this).get(LoginActivityViewModel::class.java)
         sharedPreferencesService = SharedPreferencesService(this)
+        usernameEditText = findViewById(R.id.usernameInputText)
+        passwordEditText = findViewById(R.id.passwordInputText)
+        loginBtn = findViewById(R.id.loginBtn)
+        usernameInputLayout = findViewById(R.id.usernameInputLayout)
+        passwordInputLayout = findViewById(R.id.passwordInputLayout)
 
+        toggleButton(loginBtn, false) // deactivate login button by default
         setListeners()
     }
 
     private fun setListeners() {
-        val loginBtn: Button = findViewById(R.id.loginBtn)
         loginBtn.setOnClickListener { executeLogin() }
 
-        val signUpText: TextView = findViewById(R.id.signUpText)
-        signUpText.setOnClickListener { redirectTo(this, SignUpActivity::class.java) }
+        val registerBtn: Button = findViewById(R.id.registerBtn)
+        registerBtn.setOnClickListener { redirectTo(this, SignUpActivity::class.java) }
 
-        val editText: EditText = findViewById(R.id.usernameInput)
-        onEnterKeyPressed(editText) { executeLogin() }
+        onEnterKeyPressed(usernameEditText) { executeLogin() }
+        onEnterKeyPressed(passwordEditText) { executeLogin() }
 
         val loginMain: ConstraintLayout = findViewById(R.id.loginMain)
         loginMain.setOnTouchListener { v, event -> closeKeyboard(this) }
+
+        // inputs error handling
+        usernameEditText.doOnTextChanged { text, start, before, count ->  handleInputError(text, usernameInputLayout) }
+        passwordEditText.doOnTextChanged { text, start, before, count ->  handleInputError(text, passwordInputLayout) }
+    }
+
+    // handles UI error messages depending on form errors
+    private fun handleInputError(text: CharSequence?, inputLayout: TextInputLayout) {
+        val message = if (text!!.contains(" ")) "No spaces are allowed!" else ""
+        inputLayout.error = message
+
+        // contains some kind of error
+        val containsError = usernameInputLayout.error.isNullOrEmpty() && passwordInputLayout.error.isNullOrEmpty()
+
+        val invalidInputLength = usernameEditText.text.isNullOrEmpty() || passwordEditText.text.isNullOrEmpty()
+
+        // deactivate login button if form contains error or isEmpty
+        toggleButton(loginBtn, containsError && !invalidInputLength)
     }
 
     private fun executeLogin() {
-        val usernameInput: TextView = findViewById(R.id.usernameInput)
-        val user = User(usernameInput.text.toString(), "kesh")
-
-        // check if input is valid
-        if (user.username.trim().length === 0) {
-            printToast(applicationContext, "Error! Please enter a valid username!")
-            return
-        }
+        val user = User(usernameEditText.text.toString(), passwordEditText.text.toString())
 
         // username ok -> make HTTP POST request
         val loginObserver = loginViewModel.loginUser(user)
--- mobile/app/src/main/java/com/example/colorimagemobile/utils/CommonFun.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/utils/CommonFun.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
@@ -45,6 +46,13 @@ class CommonFun {
                 }
                 return@OnKeyListener false
             })
+        }
+
+        // enables or disables button
+        fun toggleButton(button: Button, shouldEnable: Boolean) {
+            button.alpha = if (shouldEnable) 1f else .4f
+            button.isClickable = shouldEnable
+            button.isEnabled = shouldEnable
         }
     }
 }
