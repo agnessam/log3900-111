a06bbce2ce44f638b99947757f5d06335eb74fa8
keshav <kdooleea@yahoo.ca>
2021-10-09 16:40:44 -0400

    separated canSubmit logic from validator class

--- mobile/app/src/main/java/com/example/colorimagemobile/classes/FormValidator.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/classes/FormValidator.kt
@@ -3,13 +3,12 @@ package com.example.colorimagemobile.classes
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class FormValidator(layouts: ArrayList<TextInputLayout>, inputs: ArrayList<TextInputEditText>, requiredString: String) {
+class FormValidator(layouts: ArrayList<TextInputLayout>, inputs: ArrayList<TextInputEditText>) {
     private var layouts: ArrayList<TextInputLayout> = layouts
     private var inputs: ArrayList<TextInputEditText> = inputs
-    private var requiredString: String = requiredString
 
     // contains some kind of error in the form
-    private fun containsError(): Boolean {
+    fun containsError(): Boolean {
         for (layout in this.layouts) {
             // return since we know there is an error
             if (!layout.error.isNullOrEmpty()) {
@@ -21,7 +20,7 @@ class FormValidator(layouts: ArrayList<TextInputLayout>, inputs: ArrayList<TextI
     }
 
     // check if inputs are empty
-    private fun isInputEmpty(): Boolean {
+    fun isInputEmpty(requiredString: String): Boolean {
         var isInputInvalid: Boolean = false
 
         this.inputs.forEachIndexed { index, input ->
@@ -39,8 +38,4 @@ class FormValidator(layouts: ArrayList<TextInputLayout>, inputs: ArrayList<TextI
         return isInputInvalid
     }
 
-    // activate/deactivate login button if form contains error or one of the inputs is empty
-    fun canSubmit(): Boolean {
-        return !containsError() && !isInputEmpty()
-    }
 }
