c2d7dc899d9672942907c9bd8a42e96228d70016
jefferson-lam <jefferson.lam@polymtl.ca>
2021-10-12 15:10:21 -0400

    Implemented basic logout route

--- server/app/api/controllers/authentication.controller.ts
+++ server/app/api/controllers/authentication.controller.ts
@@ -1,63 +1,21 @@
-import { Router } from 'express';
-import { controller, httpPost } from 'inversify-express-utils';
+import { Request, Response } from 'express';
+import {
+	controller,
+	httpPost,
+	request,
+	response,
+} from 'inversify-express-utils';
 import { authLoginMiddleware } from '../middleware/auth_middleware';
 
 @controller('/auth')
 export class AuthenticationController {
-	onlineUsers: Set<string>;
-	router: Router;
-
-	constructor() {
-		this.onlineUsers = new Set();
-		// this.configureRouter();
-	}
-
 	@httpPost('/login', authLoginMiddleware)
 	public login() {}
 
-	// private configureRouter() {
-	// 	this.router = Router();
-
-	// 	this.router.post('/login', async (req, res, next) => {
-	// 		passport.authenticate('login', async (err, user, info) => {
-	// 			try {
-	// 				if (err || !user) {
-	// 					return res.json({
-	// 						username: null,
-	// 						token: null,
-	// 						error: info,
-	// 					});
-	// 				}
-
-	// 				req.login(user, async (error) => {
-	// 					console.log(user + ' just logged in!');
-	// 					const body = {
-	// 						_id: user._id,
-	// 						username: user.username,
-	// 					};
-	// 					const token = jwt.sign({ user: body }, 'TOP_SECRET');
-
-	// 					return res.json({
-	// 						username: user,
-	// 						token: token,
-	// 						error: err,
-	// 					});
-	// 				});
-	// 			} catch (error) {
-	// 				return next(error);
-	// 			}
-	// 		})(req, res, next);
-	// 	});
-
-	// 	this.router.post('/logout', async (req, res, next) => {
-	// 		this.onlineUsers.delete(req.body.username);
-	// 		req.logout();
-	// 		console.log(req.body.username + ' just logged out.');
-	// 		console.log('Online users: ');
-	// 		this.onlineUsers.forEach((user) => {
-	// 			console.log(user);
-	// 		});
-	// 		return res.send(true);
-	// 	});
-	// }
+	@httpPost('/logout')
+	public logout(@request() req: Request, @response() res: Response) {
+		console.log(req.body.username + ' just logged out.');
+		req.logout();
+		res.send(true);
+	}
 }
