50a11a8f327430e430faf9c3f59a7f27eca14405
YangHee-Min <heemin99@gmail.com>
2021-10-15 12:42:06 -0400

    Revert "Put tools as seperate module"
    This reverts commit a2c26109cc93ec0be8a6d467154a73ea08cf3182.

--- client/src/aapp/modules/parameter-menu/services/parameter-component/parameter-component.service.ts
+++ client/src/aapp/modules/parameter-menu/services/parameter-component/parameter-component.service.ts
@@ -1,22 +1,22 @@
 import { Injectable, Type } from "@angular/core";
 import { ControlMenuComponent } from "src/app/components/control-menu/control-menu.component";
-import { ApplierToolParameterComponent } from "../../../tool-parameters/components/applier-tool-parameter/applier-tool-parameter.component";
-import { BrushToolParameterComponent } from "../../../tool-parameters/components/brush-tool-parameter/brush-tool-parameter.component";
-import { BucketFillToolParameterComponent } from "../../../tool-parameters/components/bucket-fill-tool-parameter/bucket-fill-tool-parameter.component";
-import { EllipseToolParameterComponent } from "../../../tool-parameters/components/ellipse-tool-parameter/ellipse-tool-parameter.component";
-import { EraserToolParameterComponent } from "../../../tool-parameters/components/eraser-tool-parameter/eraser-tool-parameter.component";
-import { FeatherToolParameterComponent } from "../../../tool-parameters/components/feather-tool-parameter/feather-tool-parameter.component";
-import { GridParameterComponent } from "../../../tool-parameters/components/grid-parameter/grid-parameter.component";
-import { LineToolParameterComponent } from "../../../tool-parameters/components/line-tool-parameter/line-tool-parameter.component";
-import { PenToolParameterComponent } from "../../../tool-parameters/components/pen-tool-parameter/pen-tool-parameter.component";
-import { PencilToolParameterComponent } from "../../../tool-parameters/components/pencil-tool-parameter/pencil-tool-parameter.component";
-import { PipetteToolParameterComponent } from "../../../tool-parameters/components/pipette-tool-parameter/pipette-tool-parameter.component";
-import { PolygonToolParameterComponent } from "../../../tool-parameters/components/polygon-tool-parameter/polygon-tool-parameter.component";
-import { RectangleToolParameterComponent } from "../../../tool-parameters/components/rectangle-tool-parameter/rectangle-tool-parameter.component";
-import { SelectionToolParameterComponent } from "../../../tool-parameters/components/selection-tool-parameter/selection-tool-parameter.component";
-import { SprayToolParameterComponent } from "../../../tool-parameters/components/spray-tool-parameter/spray-tool-parameter.component";
-import { StampToolParameterComponent } from "../../../tool-parameters/components/stamp-tool-parameter/stamp-tool-parameter.component";
-import { TextToolParameterComponent } from "../../../tool-parameters/components/text-tool-parameter/text-tool-parameter.component";
+import { ApplierToolParameterComponent } from "../../../tool-parameters/applier-tool-parameter/applier-tool-parameter.component";
+import { BrushToolParameterComponent } from "../../../tool-parameters/brush-tool-parameter/brush-tool-parameter.component";
+import { BucketFillToolParameterComponent } from "../../../tool-parameters/bucket-fill-tool-parameter/bucket-fill-tool-parameter.component";
+import { EllipseToolParameterComponent } from "../../../tool-parameters/ellipse-tool-parameter/ellipse-tool-parameter.component";
+import { EraserToolParameterComponent } from "../../../tool-parameters/eraser-tool-parameter/eraser-tool-parameter.component";
+import { FeatherToolParameterComponent } from "../../../tool-parameters/feather-tool-parameter/feather-tool-parameter.component";
+import { GridParameterComponent } from "../../../tool-parameters/grid-parameter/grid-parameter.component";
+import { LineToolParameterComponent } from "../../../tool-parameters/line-tool-parameter/line-tool-parameter.component";
+import { PenToolParameterComponent } from "../../../tool-parameters/pen-tool-parameter/pen-tool-parameter.component";
+import { PencilToolParameterComponent } from "../../../tool-parameters/pencil-tool-parameter/pencil-tool-parameter.component";
+import { PipetteToolParameterComponent } from "../../../tool-parameters/pipette-tool-parameter/pipette-tool-parameter.component";
+import { PolygonToolParameterComponent } from "../../../tool-parameters/polygon-tool-parameter/polygon-tool-parameter.component";
+import { RectangleToolParameterComponent } from "../../../tool-parameters/rectangle-tool-parameter/rectangle-tool-parameter.component";
+import { SelectionToolParameterComponent } from "../../../tool-parameters/selection-tool-parameter/selection-tool-parameter.component";
+import { SprayToolParameterComponent } from "../../../tool-parameters/spray-tool-parameter/spray-tool-parameter.component";
+import { StampToolParameterComponent } from "../../../tool-parameters/stamp-tool-parameter/stamp-tool-parameter.component";
+import { TextToolParameterComponent } from "../../../tool-parameters/text-tool-parameter/text-tool-parameter.component";
 
 /// Classe permettant d'offrir dyamiquement des component selon un index
 @Injectable({
--- client/src/aapp/modules/tool-parameters/line-tool-parameter/line-tool-parameter.component.ts
+++ client/src/aapp/modules/tool-parameters/line-tool-parameter/line-tool-parameter.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
-import { RectangleStyle } from "src/app/model/rectangle-style.model";
-import { LineToolService } from "../../../tools/services/line-tool/line-tool.service";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { RectangleStyle } from 'src/app/model/rectangle-style.model';
+import { LineToolService } from '../../../services/tools/line-tool/line-tool.service';
 
 const LIGNE = 0;
 const POINTILLE_TRAIT = 1;
@@ -11,23 +11,24 @@ const EN_ANGLE = 1;
 const AVEC_POINT = 2;
 
 @Component({
-  selector: "app-line-tool-parameter",
-  templateUrl: "./line-tool-parameter.component.html",
-  styleUrls: ["./line-tool-parameter.component.scss"],
+  selector: 'app-line-tool-parameter',
+  templateUrl: './line-tool-parameter.component.html',
+  styleUrls: ['./line-tool-parameter.component.scss'],
 })
 export class LineToolParameterComponent implements OnInit {
-  constructor(private lineToolService: LineToolService) {}
+  constructor(private lineToolService: LineToolService) { }
 
   get toolName(): string {
     return this.lineToolService.toolName;
   }
 
   get strokeWidthValue(): number {
-    return (this.form.get("strokeWidth") as FormControl).value;
+    return (this.form.get('strokeWidth') as FormControl).value;
   }
 
   get diameter(): number {
-    return (this.form.get("diameter") as FormControl).value;
+
+    return (this.form.get('diameter') as FormControl).value;
   }
 
   currentStyleMotif = 0;
@@ -36,36 +37,36 @@ export class LineToolParameterComponent implements OnInit {
   motif: RectangleStyle[] = [
     {
       id: LIGNE,
-      type: "line",
-      tooltip: "ligne",
+      type: 'line',
+      tooltip: 'ligne',
     },
     {
       id: POINTILLE_TRAIT,
-      type: "largeDasharray",
-      tooltip: "pointille (trait)",
+      type: 'largeDasharray',
+      tooltip: 'pointille (trait)',
     },
     {
       id: POINTILLE_POINT,
-      type: "smallDasharray",
-      tooltip: "pointille (point)",
+      type: 'smallDasharray',
+      tooltip: 'pointille (point)',
     },
   ];
 
   jonction: RectangleStyle[] = [
     {
       id: ARRONDI,
-      type: "round",
-      tooltip: "arrondi",
+      type: 'round',
+      tooltip: 'arrondi',
     },
     {
       id: EN_ANGLE,
-      type: "angle",
-      tooltip: "en angle",
+      type: 'angle',
+      tooltip: 'en angle',
     },
     {
       id: AVEC_POINT,
-      type: "marker",
-      tooltip: "avec point",
+      type: 'marker',
+      tooltip: 'avec point',
     },
   ];
 
