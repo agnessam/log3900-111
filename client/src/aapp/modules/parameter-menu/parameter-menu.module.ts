b1f255eabe6e73e6740b38dbba09f59a54dcbfe0
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-10-15 02:28:40 -0400

    Added workspace and tool parameters modules

--- client/src/aapp/modules/parameter-menu/parameter-menu.module.ts
+++ client/src/aapp/modules/parameter-menu/parameter-menu.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MaterialModules } from "../../app-material.module";
 import { ParameterMenuComponent } from "./parameter-menu.component";
+import { WorkspaceModule } from "../workspace/workspace.module";
+import { ToolParameterModule } from "../tool-parameters/tool-parameter.module";
 
 @NgModule({
   declarations: [ParameterMenuComponent],
-  imports: [CommonModule, MaterialModules],
+  imports: [
+    CommonModule,
+    MaterialModules,
+    ToolParameterModule,
+    WorkspaceModule,
+  ],
 })
 export class ParameterMenuModule {}
--- client/src/aapp/modules/workspace/workspace.module.ts
+++ client/src/aapp/modules/workspace/workspace.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MaterialModules } from "../../app-material.module";
 import { WorkspaceComponent } from "./workspace.component";
+import { CanvasComponent } from "./components/canvas/canvas.component";
 
 @NgModule({
-  declarations: [WorkspaceComponent],
+  exports: [WorkspaceComponent],
+  declarations: [WorkspaceComponent, CanvasComponent],
   imports: [CommonModule, MaterialModules],
 })
 export class WorkspaceModule {}
