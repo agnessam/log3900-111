a2c26109cc93ec0be8a6d467154a73ea08cf3182
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-10-15 11:22:45 -0400

    Put tools as seperate module

---
+++ client/src/aapp/modules/tool-parameters/components/applier-tool-parameter/applier-tool-parameter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { ToolsApplierColorsService } from "../../../tools/services/tools-applier-colors/tools-applier-colors.service";
+
+@Component({
+  selector: "app-applier-tool-parameter",
+  templateUrl: "./applier-tool-parameter.component.html",
+  styleUrls: ["./applier-tool-parameter.component.scss"],
+})
+
+/// Le component d'affichage des paramètres du applicateur de couleur
+export class ApplierToolParameterComponent {
+  constructor(private applierToolService: ToolsApplierColorsService) {}
+
+  get toolName(): string {
+    return this.applierToolService.toolName;
+  }
+}
