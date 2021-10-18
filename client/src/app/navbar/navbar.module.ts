23565d61448eb3e7a42e0ccfd833afe49f8027d6
YangHee-Min <heemin99@gmail.com>
2021-10-18 11:51:41 -0400

    Fixed routing

--- client/src/app/navbar/navbar.module.ts
+++ client/src/app/navbar/navbar.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./navbar.component";
 import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   exports: [NavbarComponent],
   declarations: [NavbarComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
 })
 export class NavbarModule {}
