bace9281fa25767ce9c656565744efb4f42edf08
jefferson-lam <jefferson.lam@polymtl.ca>
2021-10-23 19:13:49 -0400

    Implemented basic gallery view

--- client/src/app/modules/gallery/gallery.module.ts
+++ client/src/app/modules/gallery/gallery.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from "@angular/common";
 import { GalleryComponent } from "./gallery/gallery.component";
 import { GalleryRoutingModule } from "./gallery-routing.module";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [GalleryComponent],
-  imports: [CommonModule, GalleryRoutingModule, MatGridListModule],
+  imports: [
+    CommonModule,
+    GalleryRoutingModule,
+    MatButtonModule,
+    MatGridListModule,
+  ],
 })
 export class GalleryModule {}
