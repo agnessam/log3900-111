38fd84090bad78c00b30aa771edc927f3a7239a9
Agnes <sycagnes@hotmail.com>
2021-10-23 21:35:14 -0400

    init text channel service in client

---
+++ client/src/app/modules/chat/services/text-channel.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TextChannelService {
+
+  // private endpointUrl: string = environment.serverURL + "/channels/";
+
+  constructor() { }
+}
