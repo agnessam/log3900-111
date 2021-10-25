c2a384b0c6f0e7d22316185214aa5184e5e6ba3e
jefferson-lam <jefferson.lam@polymtl.ca>
2021-10-25 00:55:40 -0400

    removed changes in client

--- client/src/app/modules/gallery/gallery.module.ts
+++ client/src/app/modules/gallery/gallery.module.ts
@@ -4,10 +4,6 @@ import { GalleryComponent } from "./gallery/gallery.component";
 import { GalleryRoutingModule } from "./gallery-routing.module";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatButtonModule } from "@angular/material/button";
-import { NewDrawingModule } from "../new-drawing/new-drawing.module";
-import { MatDialogModule } from "@angular/material/dialog";
-import { WorkspaceModule } from "../workspace/workspace.module";
-import { DrawingService } from "../workspace";
 
 @NgModule({
   declarations: [GalleryComponent],
@@ -15,11 +11,7 @@ import { DrawingService } from "../workspace";
     CommonModule,
     GalleryRoutingModule,
     MatButtonModule,
-    MatDialogModule,
     MatGridListModule,
-    NewDrawingModule,
-    WorkspaceModule,
   ],
-  providers: [DrawingService],
 })
 export class GalleryModule {}
