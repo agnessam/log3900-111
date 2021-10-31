5ccd13a3a5e07a6902806e26125bfbd820200c95
jefferson-lam <jefferson.lam@polymtl.ca>
2021-10-30 21:25:42 -0400

    Base implementation of the drawing socket service

---
+++ server/app/domain/services/sockets/drawing-socket.service.ts
@@ -0,0 +1,25 @@
+import {
+  COLLABORATIVE_DRAWING_NAMESPACE,
+  DRAW_EVENT_NAME,
+} from '../../constants/socket-constants';
+import { SocketServiceInterface } from '@app/domain/interfaces/socket.interface';
+import { injectable } from 'inversify';
+import { Server, Socket } from 'socket.io';
+
+@injectable()
+export class DrawingSocketService extends SocketServiceInterface {
+  init(io: Server) {
+    super.init(io, COLLABORATIVE_DRAWING_NAMESPACE);
+  }
+
+  protected setSocketListens(socket: Socket): void {
+    console.log(socket);
+    this.listenDrawingCommand(socket);
+  }
+
+  private listenDrawingCommand(socket: Socket): void {
+    socket.on(DRAW_EVENT_NAME, (shape) => {
+      console.log(shape);
+    });
+  }
+}
