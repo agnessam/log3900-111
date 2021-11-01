86cf4e8c9911b85f8aa5832f5a96d74bb2832113
jefferson-lam <jefferson.lam@polymtl.ca>
2021-11-01 16:39:39 -0400

    Drawing socket setup for server

--- server/app/domain/constants/socket-constants.ts
+++ server/app/domain/constants/socket-constants.ts
@@ -1,6 +1,10 @@
-export const CHAT_NAMESPACE_NAME = "chat";
-export const ROOM_EVENT_NAME = "room";
-export const TEXT_MESSAGE_EVENT_NAME = "text";
-export const CONNECTION_EVENT_NAME = "connection";
-export const DISCONNECTION_EVENT_NAME = "disconnect";
-export const LEAVE_ROOM_EVENT_NAME = "leaveRoom";
+export const CHAT_NAMESPACE_NAME = 'chat';
+export const ROOM_EVENT_NAME = 'room';
+export const TEXT_MESSAGE_EVENT_NAME = 'text';
+export const CONNECTION_EVENT_NAME = 'connection';
+export const DISCONNECTION_EVENT_NAME = 'disconnect';
+export const LEAVE_ROOM_EVENT_NAME = 'leaveRoom';
+
+// Constants for collaborative drawing
+export const COLLABORATIVE_DRAWING_NAMESPACE = 'drawing';
+export const DRAW_EVENT_NAME = 'draw';
---
+++ server/app/domain/services/sockets/drawing-socket.service.ts
@@ -0,0 +1,31 @@
+import {
+  COLLABORATIVE_DRAWING_NAMESPACE,
+  DRAW_EVENT_NAME,
+} from '../../constants/socket-constants';
+import { SocketServiceInterface } from '@app/domain/interfaces/socket.interface';
+import { injectable } from 'inversify';
+import { Server, Socket } from 'socket.io';
+import { DrawingCommand } from '@app/domain/interfaces/drawing-command.interface';
+
+@injectable()
+export class DrawingSocketService extends SocketServiceInterface {
+  init(io: Server) {
+    super.init(io, COLLABORATIVE_DRAWING_NAMESPACE);
+  }
+
+  protected setSocketListens(socket: Socket): void {
+    this.listenDrawingCommand(socket);
+  }
+
+  private listenDrawingCommand(socket: Socket): void {
+    socket.on(DRAW_EVENT_NAME, (drawingCommand: DrawingCommand) => {
+      this.emitDrawingCommand(drawingCommand);
+    });
+  }
+
+  private emitDrawingCommand(drawingCommand: DrawingCommand): void {
+    this.namespace
+      .to(drawingCommand.roomName)
+      .emit(DRAW_EVENT_NAME, drawingCommand);
+  }
+}
