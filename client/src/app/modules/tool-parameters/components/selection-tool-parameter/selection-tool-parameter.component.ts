9e5a19ba117e0494e533e0a95aa18cf7770a951e
Agnes Sam Yue Chi <agnes.sam-yue-chi@polymtl.ca>
2021-11-04 00:26:14 +0000

    Revert "Merge branch 'refactor/copy-paste' into 'dev'"
    This reverts merge request !66

--- client/src/app/modules/tool-parameters/components/selection-tool-parameter/selection-tool-parameter.component.ts
+++ client/src/app/modules/tool-parameters/components/selection-tool-parameter/selection-tool-parameter.component.ts
@@ -1,26 +1,50 @@
-import { Component } from "@angular/core";
-import {
+import { Component } from '@angular/core';
+import { CopyPasteToolService,
   DeletingToolService,
-  SelectionToolService,
-} from "src/app/modules/workspace";
+  SelectionToolService } from 'src/app/modules/workspace';
 
 @Component({
-  selector: "app-selection-tool-parameter",
-  templateUrl: "./selection-tool-parameter.component.html",
-  styleUrls: ["./selection-tool-parameter.component.scss"],
+  selector: 'app-selection-tool-parameter',
+  templateUrl: './selection-tool-parameter.component.html',
+  styleUrls: ['./selection-tool-parameter.component.scss'],
 })
 export class SelectionToolParameterComponent {
+
   constructor(
     private selectionService: SelectionToolService,
-    private deletingService: DeletingToolService
-  ) {}
+    private deletingService: DeletingToolService,
+    private copyPasteService: CopyPasteToolService) { }
 
   get toolName(): string {
     return this.selectionService.toolName;
   }
 
+  get hasClipboardObject(): boolean {
+    return this.copyPasteService.hasClipboardObject();
+  }
+
   get hasSelection(): boolean {
     return this.selectionService.hasSelection();
+  }
+
+  /// Copy
+  copy(): void {
+    this.copyPasteService.copy();
+  }
+
+  /// Cut
+  cut(): void {
+    this.copyPasteService.cut();
+  }
+
+  /// Paste
+  paste(): void {
+    this.copyPasteService.paste();
+  }
+
+  /// Duplicate
+  duplicate(): void {
+    this.copyPasteService.duplicate();
   }
 
   /// SelectAll
