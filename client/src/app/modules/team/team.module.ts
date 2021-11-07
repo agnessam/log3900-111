023b392101cb8a672350f43f0f9d0c3b9442cea6
jefferson-lam <jefferson.lam@polymtl.ca>
2021-11-07 00:09:14 -0400

    new team dialog component

--- client/src/app/modules/team/team.module.ts
+++ client/src/app/modules/team/team.module.ts
@@ -8,9 +8,10 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { TeamProfileComponent } from './team-profile/team-profile/team-profile.component';
+import { NewTeamComponent } from './new-team/new-team/new-team.component';
 
 @NgModule({
-  declarations: [TeamMainPageComponent, TeamProfileComponent],
+  declarations: [TeamMainPageComponent, TeamProfileComponent, NewTeamComponent],
   imports: [
     CommonModule,
     TeamRoutingModule,
