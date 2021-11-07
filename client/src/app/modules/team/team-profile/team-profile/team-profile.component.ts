96f8426937c90139f006b20f1f468afef1523178
jefferson-lam <jefferson.lam@polymtl.ca>
2021-11-07 00:01:04 -0400

    Complete the team profile component

--- client/src/app/modules/team/team-profile/team-profile/team-profile.component.ts
+++ client/src/app/modules/team/team-profile/team-profile/team-profile.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { TeamClientService } from "src/app/modules/backend-communication/team-client/team-client.service";
 
 @Component({
-  selector: 'app-team-profile',
-  templateUrl: './team-profile.component.html',
-  styleUrls: ['./team-profile.component.scss']
+  selector: "app-team-profile",
+  templateUrl: "./team-profile.component.html",
+  styleUrls: ["./team-profile.component.scss"],
 })
 export class TeamProfileComponent implements OnInit {
+  teamId: string;
+  team: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private teamClient: TeamClientService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.teamId = params["id"];
+      this.teamClient.getTeam(this.teamId).subscribe((team) => {
+        this.team = team;
+      });
+    });
   }
-
 }
