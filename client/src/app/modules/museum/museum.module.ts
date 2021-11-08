997e9d71bf2a162a591df460471dd6490f346653
Melody <melo25roy@gmail.com>
2021-11-08 11:19:15 -0500

    add module

---
+++ client/src/app/modules/museum/museum.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MuseumRoutingModule } from './museum-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MuseumRoutingModule
+  ]
+})
+export class MuseumModule { }
