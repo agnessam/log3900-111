b8a96e2e0f98a75b200055cffc77db86c636372c
keshav <kdooleea@yahoo.ca>
2021-11-09 08:40:27 -0500

    added border styles in view

--- mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/drawing/attributes/rectangle/RectangleFragment.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/drawing/attributes/rectangle/RectangleFragment.kt
@@ -5,56 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import android.widget.RadioGroup
 import com.example.colorimagemobile.R
+import com.example.colorimagemobile.services.drawing.toolsAttribute.PencilService
+import com.example.colorimagemobile.utils.CommonFun.Companion.printMsg
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RectangleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RectangleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var borderWidthPicker: NumberPicker
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_rectangle, container, false)
+        onRectRadioListener(view)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBorderWidthPicker(view)
+    }
+
+    private fun setBorderWidthPicker(view: View) {
+        borderWidthPicker = view.findViewById(R.id.rec_border_width_picker)
+        borderWidthPicker.minValue = PencilService.minWidth
+        borderWidthPicker.maxValue = PencilService.maxWidth
+        borderWidthPicker.value = PencilService.currentWidth
+
+        borderWidthPicker.setOnValueChangedListener { numberPicker, oldValue, newValue ->
+            printMsg(newValue.toString() + " new border")
+//            PencilService.currentWidth = newValue
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rectangle, container, false)
-    }
+    fun onRectRadioListener(view: View) {
+        val radioGroup: RadioGroup = view.findViewById(R.id.rectangle_radioGroup)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RectangleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RectangleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        radioGroup.setOnCheckedChangeListener { _, checkedId -> // checkedId is the RadioButton selected
+            when (checkedId) {
+                R.id.rect_border_fill -> printMsg("fill")
+                R.id.rect_no_border -> printMsg("no border")
+                R.id.rect_only_border -> printMsg("onlyyy border")
             }
+        }
     }
 }
