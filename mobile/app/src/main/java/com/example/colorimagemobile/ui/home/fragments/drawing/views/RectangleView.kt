4d0c19baf6b79e57464c46ef16176f7054d2e9ac
keshav <kdooleea@yahoo.ca>
2021-11-09 09:11:16 -0500

    added rectangle drawing logic but not in preview

--- mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/drawing/views/RectangleView.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/drawing/views/RectangleView.kt
@@ -1,21 +1,53 @@
 package com.example.colorimagemobile.ui.home.fragments.drawing.views
 
 import android.content.Context
+import android.graphics.Path
+import com.example.colorimagemobile.classes.toolsCommand.PencilCommand
+import com.example.colorimagemobile.classes.toolsCommand.RectangleCommand
+import com.example.colorimagemobile.services.UUIDService
+import com.example.colorimagemobile.services.drawing.CustomPaint
+import com.example.colorimagemobile.services.drawing.PaintPath
+import com.example.colorimagemobile.services.drawing.Point
+import com.example.colorimagemobile.services.drawing.toolsAttribute.ColorService
+import com.example.colorimagemobile.services.drawing.toolsAttribute.RectangleService
+import kotlin.math.abs
 
 class RectangleView(context: Context?): CanvasView(context) {
+    private var paintPath: PaintPath? = null
+    private var rectangleCommand: RectangleCommand? = null
+    private val rectangleType = "Rectangle"
+
     override fun createPathObject() {
-        TODO("Not yet implemented")
+        paintPath = PaintPath(UUIDService.generateUUID(), CustomPaint(), Path(), arrayListOf())
+        paintPath!!.brush.setStrokeWidth(RectangleService.getCurrentWidthAsFloat())
+        paintPath!!.brush.setColor(ColorService.getColorAsInt())
+
+        rectangleCommand = RectangleCommand(paintPath as PaintPath)
     }
 
     override fun onTouchDown() {
-        TODO("Not yet implemented")
+        createPathObject()
+        paintPath!!.path.moveTo(motionTouchEventX, motionTouchEventY)
+
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+        rectangleCommand!!.setStartPoint(Point(currentX, currentY))
     }
 
     override fun onTouchMove() {
-        TODO("Not yet implemented")
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+
+        // check if finger has moved for real
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            rectangleCommand!!.setEndPoint(Point(currentX, currentY))
+            rectangleCommand!!.execute()
+        }
     }
 
     override fun onTouchUp() {
-        TODO("Not yet implemented")
+        paintPath = null
     }
 }
