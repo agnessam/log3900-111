796a22d026133b216c027300bfe0682af4253feb
BANCE HALIMA DEDE BEATRICE <halima-dede-beatrice.bance@polymtl.ca>
2021-11-09 10:27:59 -0500

    add data class for HHTResponseModel and MessageModel

--- mobile/app/src/main/java/com/example/colorimagemobile/models/HTTPResponseModel.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/models/HTTPResponseModel.kt
@@ -7,4 +7,10 @@ class HTTPResponseModel {
 
     data class GetUser(val user: UserModel.AllInfo, val err: String)
     data class UpdateUser(val user: UserModel.AllInfo, val err: String)
+
+    data class GetChatChannel(val channelInfo : TextChannelModel.TextChannelAllInfo, val err : String)
+    data class DeleteChatChannel(val channelInfo : TextChannelModel.TextChannelAllInfo, val err : String )
+
+    data class GetChatMessage(val chatMessage : MessageModel.MessageAllInfo , val err : String )
+    data class GetChatMessageHistory (val channelName : String, val chatMessage : MessageModel.MessageAllInfo , val err : String )
 }
