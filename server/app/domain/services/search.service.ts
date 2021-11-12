700324ecfd188810efaf9bef05a4af689363b312
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-11-11 19:07:49 -0500

    Fixed date

--- server/app/domain/services/search.service.ts
+++ server/app/domain/services/search.service.ts
@@ -12,6 +12,21 @@ import { DrawingInterface } from '../models/Drawing';
 import { TeamInterface } from '../models/teams';
 import { UserInterface } from '../models/user';
 
+const MONTH = {
+  january: 0,
+  february: 1,
+  march: 2,
+  april: 3,
+  may: 4,
+  june: 5,
+  july: 6,
+  august: 7,
+  september: 8,
+  october: 9,
+  november: 10,
+  december: 11,
+};
+
 @injectable()
 export class SearchService implements SearchServiceInterface {
   @drawingRepository private drawingRepository: DrawingRepository;
@@ -19,6 +34,8 @@ export class SearchService implements SearchServiceInterface {
   @userRepository private userRepository: UserRepository;
 
   async search(query: string): Promise<any> {
+    const date = this.extractDate(query);
+
     const users = await this.userRepository.findManyByQuery({
       $or: [
         { username: { $regex: new RegExp(query, 'ig') } },
@@ -44,11 +61,16 @@ export class SearchService implements SearchServiceInterface {
       matchingDrawingIds,
     );
 
-    const drawings = await this.drawingRepository.findManyByQuery({
-      name: {
-        $regex: new RegExp(query, 'ig'),
-      },
-    });
+    let drawingQuery: any = {
+      $or: [{ name: { $regex: new RegExp(query, 'ig') } }],
+    };
+
+    if (date) {
+      const endDate = new Date(date.getFullYear(), date.getMonth() + 1);
+      drawingQuery.$or.push({ createdAt: { $gte: date, $lte: endDate } });
+    }
+
+    const drawings = await this.drawingRepository.findManyByQuery(drawingQuery);
 
     return {
       users: [...users],
@@ -79,5 +101,16 @@ export class SearchService implements SearchServiceInterface {
     }
 
     return uniqueDrawings;
+  }
+
+  private extractDate(query: string): Date | null {
+    // Check if it's a month
+    let month = null;
+    if (query.toLowerCase() in MONTH) {
+      month = MONTH[query];
+      return new Date(2021, month);
+    }
+
+    return null;
   }
 }
