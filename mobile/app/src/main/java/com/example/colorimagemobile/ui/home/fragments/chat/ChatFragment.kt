7de5a1acc8a638ccf82b81af1a90e40d25b0c577
keshav <kdooleea@yahoo.ca>
2021-11-13 02:22:52 -0500

    added tab to chat

--- mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/chat/ChatFragment.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/chat/ChatFragment.kt
@@ -3,45 +3,42 @@ package com.example.colorimagemobile.ui.home.fragments.chat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.colorimagemobile.R
-import com.example.colorimagemobile.adapter.ChannelListRecyclerAdapter
+import com.example.colorimagemobile.adapter.ChatChannelAdapter
 import com.example.colorimagemobile.classes.MyFragmentManager
-import com.example.colorimagemobile.models.TextChannelModel
-import com.example.colorimagemobile.repositories.TextChannelRepository
-import com.example.colorimagemobile.services.UserService
-import com.example.colorimagemobile.services.chat.TextChannelService
+import com.google.android.material.tabs.TabLayout
 
 class ChatFragment : Fragment(R.layout.fragment_chat) {
-
-    private lateinit var channels: List<TextChannelModel.AllInfo>
-    private lateinit var myView: View
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        myView = view
-        getAllChannels(UserService.getToken())
+        setChannelTabs(view)
         MyFragmentManager(requireActivity()).open(R.id.chat_channel_framelayout, ChatWelcomeFragment())
     }
 
-    private fun getAllChannels(token: String) {
-        TextChannelRepository().getAllTextChannel(token).observe(context as LifecycleOwner, {
-            if (it.isError as Boolean) {
-                return@observe
+    private fun setChannelTabs(view: View) {
+        val tabLayout = view.findViewById<TabLayout>(R.id.chat_channel_tablayout)
+        val viewPager = view.findViewById<ViewPager2>(R.id.chat_view_pager)
+
+        viewPager.adapter = ChatChannelAdapter(parentFragmentManager, requireActivity().lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("All"))
+        tabLayout.addTab(tabLayout.newTab().setText("Connected"))
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
             }
-
-            channels = it.data as List<TextChannelModel.AllInfo>
-            TextChannelService.setChannels(channels)
-            setRecyclerView()
+            override fun onTabReselected(tab: TabLayout.Tab?) { }
+            override fun onTabUnselected(tab: TabLayout.Tab?) { }
         })
-    }
 
-    private fun setRecyclerView() {
-        val recyclerView = myView.findViewById<RecyclerView>(R.id.ChannelRecycleView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = ChannelListRecyclerAdapter()
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
---
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/chat/ConnectedChannelsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.colorimagemobile.ui.home.fragments.chat
+
+import androidx.fragment.app.Fragment
+import com.example.colorimagemobile.R
+
+class ConnectedChannelsFragment : Fragment(R.layout.fragment_connected_channels) {
+
+}
