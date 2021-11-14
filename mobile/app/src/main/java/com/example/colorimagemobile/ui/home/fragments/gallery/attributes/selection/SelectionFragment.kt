cad0012039649fa99bc73c4dac38092ec08a0c63
Agnes <sycagnes@hotmail.com>
2021-11-14 01:47:34 -0500

    init selection files and fixed icons color

---
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/gallery/attributes/selection/SelectionFragment.kt
@@ -0,0 +1,17 @@
+package com.example.colorimagemobile.ui.home.fragments.gallery.attributes.selection
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.colorimagemobile.R
+import com.example.colorimagemobile.services.drawing.toolsAttribute.PencilService
+
+class SelectionFragment : Fragment(R.layout.fragment_selection) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ mobile/app/src/main/java/com/example/colorimagemobile/ui/home/fragments/gallery/views/SelectionView.kt
@@ -0,0 +1,24 @@
+package com.example.colorimagemobile.ui.home.fragments.gallery.views
+
+import android.content.Context
+import com.example.colorimagemobile.classes.toolsCommand.SelectionCommand
+
+class SelectionView(context: Context?): CanvasView(context) {
+    private var selectionCommand: SelectionCommand? = null
+
+    override fun createPathObject() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTouchDown() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTouchMove() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTouchUp() {
+        TODO("Not yet implemented")
+    }
+}
