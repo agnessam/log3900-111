ab43162a577ddb541025713c74f2094fcfab667d
BANCE HALIMA DEDE BEATRICE <halima-dede-beatrice.bance@polymtl.ca>
2021-11-14 18:40:04 -0500

    Model avatr add

---
+++ mobile/app/src/main/java/com/example/colorimagemobile/models/AvatarModel.kt
@@ -0,0 +1,21 @@
+package com.example.colorimagemobile.models
+
+import com.google.gson.annotations.SerializedName
+
+class AvatarModel {
+    data class Avatar(
+        val imageUrl: String
+    )
+
+    data class AllInfo(
+        @SerializedName("_id")
+        val _id : String,
+
+        @SerializedName("imageUrl")
+        val imageUrl: String,
+
+        @SerializedName("default")
+        val default : Boolean
+    )
+
+}
