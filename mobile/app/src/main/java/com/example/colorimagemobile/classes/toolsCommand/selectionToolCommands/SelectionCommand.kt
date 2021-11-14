7c86738f46ef36c94ac38c587d5c94daf8bdd287
Agnes <sycagnes@hotmail.com>
2021-11-14 16:23:21 -0500

    init selection commands

---
+++ mobile/app/src/main/java/com/example/colorimagemobile/classes/toolsCommand/selectionToolCommands/SelectionCommand.kt
@@ -0,0 +1,19 @@
+package com.example.colorimagemobile.classes.toolsCommand
+
+import android.graphics.Path
+import android.graphics.RectF
+import android.graphics.Region
+import com.example.colorimagemobile.interfaces.ICommand
+import com.example.colorimagemobile.models.SyncUpdate
+
+class SelectionCommand: ICommand {
+
+
+    override fun update(drawingCommand: SyncUpdate) {
+        TODO("Not yet implemented")
+    }
+
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ mobile/app/src/main/java/com/example/colorimagemobile/classes/toolsCommand/selectionToolCommands/TranslateCommand.kt
@@ -0,0 +1,14 @@
+package com.example.colorimagemobile.classes.toolsCommand
+
+import com.example.colorimagemobile.interfaces.ICommand
+import com.example.colorimagemobile.models.SyncUpdate
+
+class TranslateCommand: ICommand {
+    override fun update(drawingCommand: SyncUpdate) {
+        TODO("Not yet implemented")
+    }
+
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
