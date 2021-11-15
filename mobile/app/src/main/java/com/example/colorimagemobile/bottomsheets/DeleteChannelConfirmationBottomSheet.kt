52914c6f5caa6d5a2c5742abddc20c0304e5a34d
keshav <kdooleea@yahoo.ca>
2021-11-14 23:33:13 -0500

    updated delete cancel button to borderline

--- mobile/app/src/main/java/com/example/colorimagemobile/bottomsheets/DeleteChannelConfirmationBottomSheet.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/bottomsheets/DeleteChannelConfirmationBottomSheet.kt
@@ -7,9 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.fragment.app.FragmentActivity
 import com.example.colorimagemobile.R
-import com.example.colorimagemobile.models.TextChannelModel
 import com.example.colorimagemobile.repositories.TextChannelRepository
 import com.example.colorimagemobile.services.chat.ChatService
 import com.example.colorimagemobile.services.chat.TextChannelService
@@ -17,10 +15,11 @@ import com.example.colorimagemobile.utils.CommonFun.Companion.printToast
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
 
 class DeleteChannelConfirmationBottomSheet: BottomSheetDialogFragment() {
     private lateinit var cancelBtn: Button
-    private lateinit var deleteBtn: Button
+    private lateinit var deleteBtn: MaterialButton
     private lateinit var dialog: BottomSheetDialog
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
