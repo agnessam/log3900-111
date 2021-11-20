62bc048b67faf6069abf1cfe3767dc6cfab316ef
keshav <kdooleea@yahoo.ca>
2021-11-19 23:38:27 -0500

    fixed color opacity for sync

--- mobile/app/src/main/java/com/example/colorimagemobile/services/drawing/toolsAttribute/ColorService.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/services/drawing/toolsAttribute/ColorService.kt
@@ -1,7 +1,9 @@
 package com.example.colorimagemobile.services.drawing.toolsAttribute
 
 import android.graphics.Color
+import com.example.colorimagemobile.utils.CommonFun.Companion.printMsg
 import com.example.colorimagemobile.utils.Constants
+import java.lang.StringBuilder
 
 object ColorService {
     private var primaryColor: String = Constants.DRAWING.PRIMARY_COLOR
@@ -35,7 +37,7 @@ object ColorService {
     fun rgbaToInt(color: String): Int {
         val rgbValues = color.substring(color.indexOf('(') + 1, color.indexOf(')'))
         val splitRGB = rgbValues.replace("\\s".toRegex(),"").split(",")
-        var alpha = if (splitRGB.size == 4) splitRGB[3].toInt() else 255
+        var alpha = if (splitRGB.size == 4) splitRGB[3].toInt() else Constants.DRAWING.MAX_OPACITY
 
         return Color.argb(alpha, splitRGB[0].toInt(), splitRGB[1].toInt(), splitRGB[2].toInt())
     }
@@ -51,11 +53,33 @@ object ColorService {
 
     // convert alpha from 0-255 (leger) to 0-1 (lourd)
     fun getAlphaForDesktop(color: String): String {
+        if (color == "none") return "none"
+
         val rgbaValues = color.substring(color.indexOf('(') + 1, color.indexOf(')'))
         val splitRGBA = rgbaValues.replace("\\s".toRegex(),"").split(",")
-        val currentAlpha = if (splitRGBA.size == 4) splitRGBA[3].toInt() else 255
+        val currentAlpha = if (splitRGBA.size == 4) splitRGBA[3].toInt() else Constants.DRAWING.MAX_OPACITY
 
-        val alpha = currentAlpha.toDouble() / 255
+        val alpha = currentAlpha.toDouble() / Constants.DRAWING.MAX_OPACITY
         return alpha.toString()
+    }
+
+    // adds alpha ranging from 0-1 to 0-255 to rgba string
+    fun addAlphaToRGBA(color: String, opacity: String): String {
+        if (color == "none") return "none"
+
+        val closeParenIndex = color.indexOf(")")
+        val transformedColor = StringBuilder(color)
+
+        val newOpacity = convertOpacityToAndroid(opacity)
+        transformedColor.insert(closeParenIndex, ", $newOpacity")
+
+        return transformedColor.toString()
+    }
+
+    // convert 0-1 to 0-255
+    fun convertOpacityToAndroid(opacity: String): Int {
+        if (opacity == "none") return Constants.DRAWING.MAX_OPACITY
+
+        return (opacity.toDouble() * Constants.DRAWING.MAX_OPACITY).toInt()
     }
 }
