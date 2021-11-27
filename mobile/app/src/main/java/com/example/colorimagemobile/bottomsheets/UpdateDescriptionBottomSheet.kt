3343533d3b9b376fcd3366935ae3571e9f4845c3
BANCE HALIMA DEDE BEATRICE <halima-dede-beatrice.bance@polymtl.ca>
2021-11-27 08:13:33 -0500

    update description bottomsheet create

---
+++ mobile/app/src/main/java/com/example/colorimagemobile/bottomsheets/UpdateDescriptionBottomSheet.kt
@@ -0,0 +1,46 @@
+package com.example.colorimagemobile.bottomsheets
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.colorimagemobile.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class UpdateDescriptionBottomSheet : BottomSheetDialogFragment() {
+    private lateinit var dialog: BottomSheetDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_update_description_bottom_sheet,
+            container,
+            false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        return dialog
+    }
+
+    private fun closeSheet() { dialog.behavior.state = BottomSheetBehavior.STATE_HIDDEN }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
