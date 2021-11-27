153ae05d95fb8c8e0e5f50d1073f1312bbe08bfb
BANCE HALIMA DEDE BEATRICE <halima-dede-beatrice.bance@polymtl.ca>
2021-11-27 02:53:46 -0500

    Scrollview add for team/users/gallery the other view work already

--- mobile/app/src/main/java/com/example/colorimagemobile/services/users/UserAdapterService.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/services/users/UserAdapterService.kt
@@ -2,10 +2,8 @@ package com.example.colorimagemobile.services.users
 
 import android.content.Context
 import androidx.fragment.app.FragmentActivity
-import com.example.colorimagemobile.adapter.TeamsMenuRecyclerAdapter
 import com.example.colorimagemobile.adapter.UsersMenuRecyclerAdapter
 import com.example.colorimagemobile.classes.MyFragmentManager
-import com.example.colorimagemobile.services.teams.TeamAdapterService
 import com.example.colorimagemobile.ui.home.fragments.users.UsersProfileFragment
 import com.example.colorimagemobile.utils.Constants
 
