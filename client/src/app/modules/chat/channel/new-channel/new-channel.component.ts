0981e00ba242cb3fbf9a5773f3f494f71cdb9bfb
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-11-27 01:41:37 -0500

    Uniformised the new chat dialog with the rest of the app

--- client/src/app/modules/chat/channel/new-channel/new-channel.component.ts
+++ client/src/app/modules/chat/channel/new-channel/new-channel.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { TextChannel } from '../../models/text-channel.model';
-import { TextChannelService } from '../../services/text-channel.service';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { TextChannel } from "../../models/text-channel.model";
+import { TextChannelService } from "../../services/text-channel.service";
 
 @Component({
-  selector: 'app-new-channel',
-  templateUrl: './new-channel.component.html',
-  styleUrls: ['./new-channel.component.scss']
+  selector: "app-new-channel",
+  templateUrl: "./new-channel.component.html",
+  styleUrls: ["./new-channel.component.scss"],
 })
 export class NewChannelComponent implements OnInit {
   newDrawingForm: FormGroup;
@@ -17,25 +17,31 @@ export class NewChannelComponent implements OnInit {
   constructor(
     private textChannelService: TextChannelService,
     private dialogRef: MatDialogRef<NewChannelComponent>,
-    private snackBar: MatSnackBar,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
-    this.dialogRef.updateSize("20%", "40%");
     this.newDrawingForm = new FormGroup({
       name: new FormControl(""),
     });
-    this.textChannelService.getChannels().subscribe((channels) => this.existingChannels = channels);
+    this.textChannelService
+      .getChannels()
+      .subscribe((channels) => (this.existingChannels = channels));
   }
 
   onAccept(): void {
-    const isWhitespace = (this.newDrawingForm.value.name || "").trim().length === 0;
+    const isWhitespace =
+      (this.newDrawingForm.value.name || "").trim().length === 0;
     if (isWhitespace) {
       this.snackBar.open("The name cannot be empty", "Close", {
         duration: 3000,
       });
       return;
-    } else if (this.existingChannels.find((channel) => channel.name === this.newDrawingForm.value.name)) {
+    } else if (
+      this.existingChannels.find(
+        (channel) => channel.name === this.newDrawingForm.value.name
+      )
+    ) {
       this.snackBar.open("This channel already exists", "Close", {
         duration: 3000,
       });
@@ -43,7 +49,10 @@ export class NewChannelComponent implements OnInit {
     }
 
     this.textChannelService
-      .createChannel(this.newDrawingForm.value.name, localStorage.getItem("userId")!)
+      .createChannel(
+        this.newDrawingForm.value.name,
+        localStorage.getItem("userId")!
+      )
       .subscribe((response) => {
         this.dialogRef.close(response);
         this.newDrawingForm.reset();
