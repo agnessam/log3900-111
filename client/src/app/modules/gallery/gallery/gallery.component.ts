07b4f3d837da29f05d3640e7086c9c046fc1e610
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-11-28 00:39:57 -0500

    privacy filter

--- client/src/app/modules/gallery/gallery/gallery.component.ts
+++ client/src/app/modules/gallery/gallery/gallery.component.ts
@@ -4,6 +4,8 @@ import { Component, AfterViewInit } from "@angular/core";
 import { DrawingHttpClientService } from "../../backend-communication";
 import { Drawing } from "src/app/shared";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Team } from "src/app/shared/models/team.model";
+import { User } from "../../users/models/user";
 
 @Component({
   selector: "app-gallery",
@@ -12,16 +14,21 @@ import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 })
 export class GalleryComponent implements AfterViewInit {
   drawings: Array<Drawing> = [];
+  userId: string;
   constructor(
     private dialog: MatDialog,
     private drawingHttpClient: DrawingHttpClientService,
     private sanitizer: DomSanitizer
-  ) {}
+  ) {
+    this.userId = localStorage.getItem("userId")!;
+  }
 
   ngAfterViewInit(): void {
     this.drawingHttpClient.getDrawings().subscribe((response) => {
       for (let drawing of response) {
-        this.drawings.push(drawing);
+        if (this.hasPermission(drawing)) {
+          this.drawings.push(drawing);
+        }
       }
     });
   }
@@ -32,5 +39,24 @@ export class GalleryComponent implements AfterViewInit {
 
   createNewDrawing() {
     this.dialog.open(NewDrawingComponent, {});
+  }
+
+  hasPermission(drawing: Drawing): boolean {
+    if (drawing.privacyLevel != "private") return true;
+
+    if (
+      drawing.ownerModel == "User" &&
+      (drawing.owner as User)._id == this.userId
+    ) {
+      return true;
+    }
+
+    if (drawing.ownerModel == "Team") {
+      if (((drawing.owner as Team).members as string[]).includes(this.userId)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
