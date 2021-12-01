d248d2c7d0fc29efd56f486a78f760ba8ef6e55a
BANCE HALIMA DEDE BEATRICE <halima-dede-beatrice.bance@polymtl.ca>
2021-12-01 05:34:25 -0500

    autheur post image fix

--- mobile/app/src/main/java/com/example/colorimagemobile/models/recyclerAdapters/DrawingMenu.kt
+++ mobile/app/src/main/java/com/example/colorimagemobile/models/recyclerAdapters/DrawingMenu.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import com.example.colorimagemobile.models.DrawingModel
 
 /* holds data of drawing displayed in Gallery */
