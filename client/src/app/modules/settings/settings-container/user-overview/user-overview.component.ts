810dbf3bcbad891f022e7a63d0f3da32c4756642
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-12-01 02:57:22 -0500

    Small update:)

--- client/src/app/modules/settings/settings-container/user-overview/user-overview.component.ts
+++ client/src/app/modules/settings/settings-container/user-overview/user-overview.component.ts
@@ -41,8 +41,6 @@ export class UserOverviewComponent implements OnInit {
     this.editParameterDialogRef = this.dialog.open(
       EditParameterDialogComponent,
       {
-        width: "500px",
-        height: "400px",
         data: {
           username: this.currentUser.username,
         },
@@ -59,8 +57,6 @@ export class UserOverviewComponent implements OnInit {
     this.editDescriptionDialogRef = this.dialog.open(
       EditDescriptionDialogComponent,
       {
-        width: "500px",
-        height: "400px",
         data: {
           description: this.currentUser.description,
         },
