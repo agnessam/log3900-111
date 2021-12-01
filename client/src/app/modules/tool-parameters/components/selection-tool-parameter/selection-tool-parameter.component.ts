4824ba189bde14bf0ad67c8267a8874726b7785a
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-12-01 14:28:23 -0500

    Remove unused selection stuff

--- client/src/app/modules/tool-parameters/components/selection-tool-parameter/selection-tool-parameter.component.ts
+++ client/src/app/modules/tool-parameters/components/selection-tool-parameter/selection-tool-parameter.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
 import {
-  CopyPasteToolService,
   DeletingToolService,
   SelectionToolService,
 } from "src/app/modules/workspace";
@@ -13,60 +12,22 @@ import {
 })
 export class SelectionToolParameterComponent {
   form: FormGroup;
-  private strokeWidth: FormControl;
 
   constructor(
     private selectionService: SelectionToolService,
-    private deletingService: DeletingToolService,
-    private copyPasteService: CopyPasteToolService
-  ) {
-    this.strokeWidth = new FormControl(1, Validators.min(1));
-    this.form = new FormGroup({
-      strokeWidth: this.strokeWidth,
-    });
-    this.selectionService.lineWidthSubject.subscribe((lineWidth: number) => {
-      this.strokeWidth.setValue(lineWidth);
-    });
-  }
+    private deletingService: DeletingToolService
+  ) {}
 
   get toolName(): string {
     return this.selectionService.toolName;
-  }
-
-  get hasClipboardObject(): boolean {
-    return this.copyPasteService.hasClipboardObject();
   }
 
   get hasSelection(): boolean {
     return this.selectionService.hasSelection();
   }
 
-  /// Copy
-  copy(): void {
-    this.copyPasteService.copy();
-  }
-
-  /// Cut
-  cut(): void {
-    this.copyPasteService.cut();
-  }
-
-  /// Paste
-  paste(): void {
-    this.copyPasteService.paste();
-  }
-
-  /// Duplicate
-  duplicate(): void {
-    this.copyPasteService.duplicate();
-  }
-
   /// SelectAll
   deleteSelection(): void {
     this.deletingService.deleteSelection();
-  }
-
-  changeLineWidth(): void {
-    this.selectionService.setSelectionLineWidth(this.strokeWidth.value);
   }
 }
