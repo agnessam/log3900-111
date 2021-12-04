702d4d2d3cef141b7e602cf81321562474e4cc0a
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-12-04 02:15:30 -0500

    Add default avatar for the team

--- client/src/app/modules/post-dialog/post-dialog.component.ts
+++ client/src/app/modules/post-dialog/post-dialog.component.ts
@@ -1,23 +1,29 @@
-import { PostService } from './../museum/services/post.service';
-import { Component, OnInit, Inject , ViewChild, ElementRef} from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { PostInterface } from '../museum/models/post.model';
-import { DomSanitizer } from '@angular/platform-browser';
-import { AuthenticationService } from '../authentication';
-import { User } from '../authentication/models/user';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { CommentInterface } from '../museum/models/comment.model';
-import { Team } from 'src/app/shared/models/team.model';
-
+import { PostService } from "./../museum/services/post.service";
+import {
+  Component,
+  OnInit,
+  Inject,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { PostInterface } from "../museum/models/post.model";
+import { DomSanitizer } from "@angular/platform-browser";
+import { AuthenticationService } from "../authentication";
+import { User } from "../authentication/models/user";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { CommentInterface } from "../museum/models/comment.model";
+import { Team } from "src/app/shared/models/team.model";
 
 @Component({
-  selector: 'app-post-dialog',
-  templateUrl: './post-dialog.component.html',
-  styleUrls: ['./post-dialog.component.scss']
+  selector: "app-post-dialog",
+  templateUrl: "./post-dialog.component.html",
+  styleUrls: ["./post-dialog.component.scss"],
 })
 export class PostDialogComponent implements OnInit {
-  @ViewChild("commentInput", { static: false }) private commentInput: ElementRef<HTMLInputElement>;
-  @ViewChild('audioOption') audioPlayerRef: ElementRef;
+  @ViewChild("commentInput", { static: false })
+  private commentInput: ElementRef<HTMLInputElement>;
+  @ViewChild("audioOption") audioPlayerRef: ElementRef;
 
   user: User | null;
 
@@ -25,50 +31,50 @@ export class PostDialogComponent implements OnInit {
     public dialogRef: MatDialogRef<PostDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public post: PostInterface,
     private sanitizer: DomSanitizer,
-    private postService:PostService,
+    private postService: PostService,
     private authenticationService: AuthenticationService,
-    private snackBar: MatSnackBar,
-    ) { }
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.dialogRef.backdropClick().subscribe(_ => {
+    this.dialogRef.backdropClick().subscribe((_) => {
       this.dialogRef.close(this.post);
-    })
+    });
     this.authenticationService.currentUserObservable.subscribe(
-      (user) => (this.user = user),
+      (user) => (this.user = user)
     );
   }
 
-  transformUri(){
+  transformUri() {
     let safeDataUri = this.sanitizer.bypassSecurityTrustUrl(this.post.dataUri);
     return safeDataUri;
   }
 
-  toggleLike(){
+  toggleLike() {
     const userid = (this.user as User)._id;
 
-    if (this.post.likes.includes(userid)){
+    if (this.post.likes.includes(userid)) {
       this.postService.removeLike(userid, this.post._id).subscribe((like) => {
-        const index = this.post.likes.findIndex(like => like === userid) as number;
+        const index = this.post.likes.findIndex(
+          (like) => like === userid
+        ) as number;
         this.post.likes.splice(index, 1);
       });
-    }
-    else{
+    } else {
       this.onAudioPlay();
       this.postService.addLike(userid, this.post._id).subscribe((like) => {
         this.post.likes.push(userid);
       });
-
     }
   }
 
-  hasBeenLike(likes: string[]): boolean{
-    if(this.user?._id == null) return false;
+  hasBeenLike(likes: string[]): boolean {
+    if (this.user?._id == null) return false;
     return likes.includes(this.user?._id);
   }
 
-  addComment(){
-    if(this.user?._id == null) return;
+  addComment() {
+    if (this.user?._id == null) return;
     let input = this.commentInput.nativeElement.value;
     this.commentInput.nativeElement.value = "";
 
@@ -79,43 +85,48 @@ export class PostDialogComponent implements OnInit {
       });
       return;
     }
-    const comment = { content: input, author: this.user, postId: this.post._id} as CommentInterface;
-    this.postService.addComment(this.post._id, comment).subscribe((commentReceive) => {
-      this.post.comments.push(comment)
-    });
+    const comment = {
+      content: input,
+      author: this.user,
+      postId: this.post._id,
+    } as CommentInterface;
+    this.postService
+      .addComment(this.post._id, comment)
+      .subscribe((commentReceive) => {
+        this.post.comments.push(comment);
+      });
   }
 
-  getAuthorUsername(comment: CommentInterface){
+  getAuthorUsername(comment: CommentInterface) {
     let author = comment.author as User;
     return author.username;
   }
 
-  getUsername(post: PostInterface): string{
-    if( post.ownerModel === "Team"){
+  getUsername(post: PostInterface): string {
+    if (post.ownerModel === "Team") {
       let owner = post.owner as Team;
       return owner.name;
-    }
-    else{
+    } else {
       let owner = post.owner as User;
       return owner.username;
     }
-
   }
 
-  getAvatar(post: PostInterface): string{
-    if( post.ownerModel === "User"){
+  getAvatar(post: PostInterface): string {
+    if (post.ownerModel === "User") {
       let owner = post.owner as User;
       return owner.avatar.imageUrl;
+    } else if (post.ownerModel === "Team") {
+      return (post.owner as Team).name;
     }
     return "";
   }
 
-  getTime(createdAt: string): string{
+  getTime(createdAt: string): string {
     return createdAt.split("T")[0];
   }
 
-  onAudioPlay(){
+  onAudioPlay() {
     this.audioPlayerRef.nativeElement.play();
   }
-
 }
